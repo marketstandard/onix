4c4aee17860d54649b1937faeaa75b3e38522f88
Trey Huffine <treyhuffine@gmail.com>
2025-01-28 14:10:38 -0500

    Store crypto in JWT

--- src/components/ConnectWallet/ConnectWallet.tsx
+++ src/components/ConnectWallet/ConnectWallet.tsx
@@ -1,16 +1,43 @@
+import { useEffect } from 'react';
 import { ArrowLeftOnRectangleIcon, WalletIcon } from '@heroicons/react/24/outline';
 import { useWallet } from '@solana/wallet-adapter-react';
 import { useWalletModal } from '@solana/wallet-adapter-react-ui';
+import { signOut, useSession } from 'next-auth/react';
+import { signInWithSolana } from 'utils/client/wallet';
 import Button from 'components/Button';
 
 interface Props {}
 
 export default function ConnectWallet({}: Props) {
-  const { connected, publicKey, disconnect } = useWallet();
+  const wallet = useWallet();
+  const { connected, publicKey, disconnect } = wallet;
   const { setVisible } = useWalletModal();
+  const { data: session, status } = useSession();
+
+  useEffect(() => {
+    const handleAuth = async () => {
+      if (connected && publicKey && !session?.user?.publicKey && status === 'unauthenticated') {
+        try {
+          console.log('--- signing in with solana');
+          await signInWithSolana(wallet);
+        } catch (error) {
+          console.error('Failed to authenticate wallet:', error);
+          // Optionally disconnect wallet on auth failure
+          // disconnect();
+        }
+      }
+    };
+
+    handleAuth();
+  }, [connected, publicKey, session, wallet, status]);
 
   const abbreviateAddress = (address: string) => {
     return `${address.slice(0, 6)}...${address.slice(-6)}`;
+  };
+
+  const handleDisconnect = async () => {
+    disconnect();
+    await signOut({ redirect: false });
   };
 
   return (
@@ -27,7 +54,7 @@ export default function ConnectWallet({}: Props) {
               {publicKey ? abbreviateAddress(publicKey.toString()) : ''}
             </span>
           </div>
-          <button onClick={disconnect} className="w-fit rounded-md bg-negative p-2">
+          <button onClick={handleDisconnect} className="w-fit rounded-md bg-negative p-2">
             <ArrowLeftOnRectangleIcon className="h-6 w-6" />
           </button>
         </div>
